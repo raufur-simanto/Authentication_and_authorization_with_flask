7087f7c5c72d6fbd87eba98516d7bf5b902eedd2
raufur <raufur.rahman@brilliant.com.bd>
2024-10-19 12:49:41 +0600

    add lambda function

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
+from aws_lambda_wsgi import response
 from project import create_app
 app = create_app()
+
+def lambda_handler(event, context):
+    """
+    Lambda handler that will handle the API Gateway events
+    """
+    return response(app, event, context)
 
 if "__name__" == "__main__":
     app.run(host='0.0.0.0', port=5000)
